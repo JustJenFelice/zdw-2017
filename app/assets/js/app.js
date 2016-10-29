f0bb90c4004609e43d90468e1bdca2e3e91639f0
Christian Jánský <chrisjansky@gmail.com>
2016-10-30 00:48:37 +0200

    PUG SCSS JS Added waypoint to o-nav

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,5 +1,15 @@
 var
+  $body = $("body");
+
+var
   endDate = moment("2017-01-31"),
   deadDate = endDate.fromNow();
 
 $("[data-countdown]").text("Uzávěrka " + deadDate);
+
+new Waypoint({
+  element: "[data-menu]",
+  handler: function(direction) {
+    $body.toggleClass("nav--is-pinned", direction == "down");
+  }
+});
