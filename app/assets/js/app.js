3283a38d55e831ef99f0345b3b22bd4174a5f712
Christian Jánský <chrisjansky@gmail.com>
2016-11-27 13:07:46 +0100

    PUG SCSS JS Changed fileUpload to multiple, limited to 20 files

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -10,3 +10,11 @@ $("[data-countdown]").text("Uzávěrka " + deadDate);
 $("[data-nav-toggle]").on("click", function() {
   $body.toggleClass("nav--is-open");
 });
+
+$("[data-upload]").change(function() {
+  if (this.files.length > 20) {
+    $("[data-upload-info]").addClass("animate--glow");
+
+    this.value = "";
+  }
+});
