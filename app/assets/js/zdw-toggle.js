fb4fe27f0b7171916dc23c8af91f90714b15cb16
Christian Jánský <chrisjansky@gmail.com>
2016-12-29 10:37:24 +0100

    PUG SCSS JS Added zdw/program

---
+++ app/assets/js/zdw-toggle.js
@@ -0,0 +1,9 @@
+var toggleObj = document.querySelectorAll('[data-toggle]');
+
+for (var i = 0; i < toggleObj.length; i++) {
+  toggleObj[i].onclick = function() {
+    var classAttr = this.getAttribute('data-toggle');
+
+    document.body.classList.toggle(classAttr + '--is-active');
+  }
+}
