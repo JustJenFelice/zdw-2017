a725c534063bea99786cfb4164eec8d7e9b1ecf7
Christian Jánský <chrisjansky@gmail.com>
2017-01-05 20:43:04 +0100

    Uninstalled glob, deleted templates:read, started using gulp-data

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -2,51 +2,25 @@ var
   gulp = require("gulp"),
   fs = require("fs"),
   plugins = require("gulp-load-plugins")({
-    pattern: ["glob", "gulp-*", "beepbeep", "browser-*"]
+    pattern: ["gulp-*", "beepbeep", "browser-*"]
   }),
   config = require("../gulpconfig.json");
 
-// Parse JSON locals.
-var jsonOutput;
-gulp.task("templates:read", function() {
-  var jsonFiles = plugins.glob.sync(config.dev.dataGlob);
-
-  // Check if files exist.
-  if (jsonFiles.length > 0) {
-
-    jsonOutput = fs.readFileSync(jsonFiles[0], "utf8");
-
-    // Add other files if more than one.
-    if (jsonFiles.length > 1) {
-      // Slicing always last two chars because of newline!
-      jsonOutput = jsonOutput.slice(0, -2);
-
-      for (i = 1; i < jsonFiles.length; i++) {
-        // Remove wrapping brackets.
-        var slicedFile = fs.readFileSync(jsonFiles[i], "utf8").slice(1, -2);
-        jsonOutput += ",\n" + slicedFile;
-      }
-
-      jsonOutput += "}";
-    }
-
-  } else {
-    jsonOutput = null;
-  }
-});
-
-gulp.task("templates:compile", ["templates:read"], function() {
+gulp.task("templates:compile", function() {
   return gulp.src([config.dev.pugGlob, config.dev.pugIgnore])
   
     .pipe(plugins.plumber(function (error) {
       plugins.beepbeep();
-      console.log(error);
+      console.log(error.message);
       this.emit("end");
+    }))
+
+    .pipe(plugins.data(function(file) {
+      return JSON.parse(fs.readFileSync(config.dev.jsonGlobal))
     }))
 
     .pipe(plugins.pug({
       pretty: true,
-      locals: JSON.parse(jsonOutput),
       basedir: config.dev.root
     }))
 
