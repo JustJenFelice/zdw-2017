d1a22a53425ad94292cd5b6f98baa718fff92a10
Christian Jánský <chrisjansky@gmail.com>
2017-01-11 16:27:34 +0100

    Added gulp test task for production version testing

--- gulpfile.js
+++ gulpfile.js
@@ -183,6 +183,20 @@ gulp.task('build:images', ['build:wipe'], function() {
 
 /*
 
+Test production version
+
+*/
+gulp.task('test', function() {
+  return browserSync.init({
+    server: {
+      baseDir: config.dist.root
+    },
+    notify: false
+  });
+});
+
+/*
+
 SVG
 
 */
